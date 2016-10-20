2856f6f3590e75a11fbfbf8fe92e86cff9a7d939
RottenWang <80250706@qq.com>
2016-10-20 22:17:53 +0800

    RxJavaDemo  IO Scheduler

--- app/src/main/java/com/drwang/rxjavademo/MainActivity.java
+++ app/src/main/java/com/drwang/rxjavademo/MainActivity.java
@@ -1,17 +1,22 @@
 package com.drwang.rxjavademo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
     private static final String TAG = "MainActivity";
+    private Subscriber<String> mSubscriber;
+    private Observable<String> mObservable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +24,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
         //1, 创建被观察的对象
-        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>(){
+        //在订阅时被调用
+        mObservable = Observable.create(new Observable.OnSubscribe<String>(){
             //在订阅时被调用
             @Override
             public void call(Subscriber<? super String> subscriber) {
+                showThread("请求数据");
+                SystemClock.sleep(5000);
+
                 subscriber.onNext("哈哈哈");
+
                 subscriber.onCompleted();
+
             }
         });
         //2, 创建观察者对象  订阅
-        Subscriber<String> subscriber = new Subscriber<String>() {
+        mSubscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
                 Log.i(TAG, "onCompleted: ");
+                showThread("onCompleted");
+
             }
 
             @Override
@@ -41,12 +54,20 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNext(String s) {
-                Log.i(TAG, "onNext: ");
                 tv.setText(s);
+                showThread("onNext");
             }
         };
-        //3, 订阅
-        observable.subscribe(subscriber);
-        
+
+        //3, 订阅 , 产生调用
+//        mObservable.subscribe(mSubscriber);
+
+        //4.异步和同步
+        mObservable.subscribeOn(Schedulers.io()) //  call方法在子线程被调用
+                .observeOn(AndroidSchedulers.mainThread())// 观察者的方法在主线程被调用
+                .subscribe(mSubscriber);
+    }
+    public void showThread(String str) {
+        Log.i(TAG, "showThread: str Thread = " + Thread.currentThread().getName() + "str = " + str);
     }
 }
